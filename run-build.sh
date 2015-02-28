6119ec4fef059168a92f9a9985d150f2d52dcaa7
sumimakito <sumimakito@hotmail.com>
2015-02-28 22:24:37 +0800

    Fixed curl

--- run-build.sh
+++ run-build.sh
@@ -1,4 +1,6 @@
 ./gradlew clean build
 cd ./library/build/libs
-curl -F "file=@`ls | grep source.jar`" http://repo.keep.moe/api/travis/push/qkv.php
-curl -F "file=@`ls | grep javadoc.jar`" http://repo.keep.moe/api/travis/push/qkv.php
+source=@`ls | grep source.jar`
+javadoc=@`ls | grep javadoc.jar`
+curl -F "file=${source}" http://repo.keep.moe/api/travis/push/qkv.php
+curl -F "file=${javadoc}" http://repo.keep.moe/api/travis/push/qkv.php
