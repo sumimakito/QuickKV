d007986babac7399560945238d09323e9bec734b
sumimakito <sumimakito@hotmail.com>
2015-02-28 21:41:03 +0800

    I'm about to be mmmmaaaaddddd!!!

--- run-build.sh
+++ run-build.sh
@@ -1,3 +1,3 @@
 ./gradlew clean build
-curl -F "file=@./library/build/libs/library-*-sources.jar" http://repo.keep.moe/api/travis/push/qkv.php
-curl -F "jar=@./library/build/libs/library-*-javadoc.jar" http://repo.keep.moe/api/travis/push/qkv.php
+curl -F "file=@./library/build/libs/library-0.6-sources.jar" http://repo.keep.moe/api/travis/push/qkv.php
+curl -F "jar=@./library/build/libs/library-0.6-javadoc.jar" http://repo.keep.moe/api/travis/push/qkv.php
