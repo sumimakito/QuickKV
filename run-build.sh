00ddcb91e45a8a29848f82566ce0cf725f1e4fb1
sumimakito <sumimakito@hotmail.com>
2015-02-28 17:03:37 +0800

    QAQ

--- run-build.sh
+++ run-build.sh
@@ -1,3 +1,3 @@
-../gradlew clean build
+gradlew clean build
 curl --form "jar=./library/build/libs/library-*-sources.jar" http://code.keep.moe/api/ci/push/QuickKV
 curl --form "jar=./library/build/libs/library-*-javadoc.jar" http://code.keep.moe/api/ci/push/QuickKV
