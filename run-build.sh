683d8d67f3a68f59ef60412d9346deac6a656fbf
SumiMakito <sumimakito@hotmail.com>
2015-07-29 23:08:28 +0800

    Update to 0.9.5

--- run-build.sh
+++ run-build.sh
@@ -1,10 +1,9 @@
 ./gradlew --stacktrace clean build
 GITHUB_TAG=Snapshot_${TRAVIS_BRANCH}_Build.${TRAVIS_BUILD_NUMBER}
 cwd="`pwd`"
-find
-cd ./library/build/libs
-sources=`ls | grep sources.jar`
-javadoc=`ls | grep javadoc.jar`
-mv ${sources} QuickKV_${GITHUB_TAG}_source.jar
-mv ${javadoc} QuickKV_${GITHUB_TAG}_javadoc.jar
+cd ./library/build/intermediates/bundles/release
+#sources=`ls | grep sources.jar`
+#javadoc=`ls | grep javadoc.jar`
+mv classes.jar QuickKV_${GITHUB_TAG}.jar
+#mv ${javadoc} QuickKV_${GITHUB_TAG}_javadoc.jar
 cd ${cwd}
