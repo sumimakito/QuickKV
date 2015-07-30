8db70ae6931fd81e196c5f781c8ba4152116333f
SumiMakito <sumimakito@hotmail.com>
2015-07-30 13:53:44 +0800

    Update jar build script

--- run-build.sh
+++ run-build.sh
@@ -1,9 +1,11 @@
 ./gradlew --stacktrace clean build
+./gradlew fatJar
 GITHUB_TAG=Snapshot_${TRAVIS_BRANCH}_Build.${TRAVIS_BUILD_NUMBER}
 cwd="`pwd`"
 cd ./library/build/libs
-sources=`ls | grep sources.jar`
-javadoc=`ls | grep javadoc.jar`
-mv ${sources} QuickKV_${GITHUB_TAG}_sources.jar
-mv ${javadoc} QuickKV_${GITHUB_TAG}_javadoc.jar
+#sources=`ls | grep sources.jar`
+#javadoc=`ls | grep javadoc.jar`
+#mv ${sources} QuickKV_${GITHUB_TAG}_sources.jar
+#mv ${javadoc} QuickKV_${GITHUB_TAG}_javadoc.jar
+mv QuickKV.jar QuickKV_${GITHUB_TAG}.jar
 cd ${cwd}
