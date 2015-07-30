a9a3761407bdc7b0d4f483d40b472c0a184ffd9d
SumiMakito <sumimakito@hotmail.com>
2015-07-30 12:06:18 +0800

    Update build script

--- run-build.sh
+++ run-build.sh
@@ -1,9 +1,10 @@
 ./gradlew --stacktrace clean build
 GITHUB_TAG=Snapshot_${TRAVIS_BRANCH}_Build.${TRAVIS_BUILD_NUMBER}
 cwd="`pwd`"
-cd ./library/build/intermediates/bundles/release
+find
+#cd ./library/build/intermediates/bundles/release
 #sources=`ls | grep sources.jar`
 #javadoc=`ls | grep javadoc.jar`
-mv classes.jar QuickKV_${GITHUB_TAG}.jar
+#mv classes.jar QuickKV_${GITHUB_TAG}.jar
 #mv ${javadoc} QuickKV_${GITHUB_TAG}_javadoc.jar
 cd ${cwd}
